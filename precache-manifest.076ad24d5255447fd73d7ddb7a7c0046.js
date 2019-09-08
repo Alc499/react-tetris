7ddd6064adf4f106630a74e6e75853c1ab03c41a
Alc499 <Ali.vitchenko@gmail.com>
2019-09-08 22:37:31 +0300

    Updates

---
+++ precache-manifest.076ad24d5255447fd73d7ddb7a7c0046.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "296a2c7f3f9fc32baba090f95ef45692",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "45aebee8ce87cf8f24dd",
+    "url": "/react-tetris/static/css/main.02c38205.chunk.css"
+  },
+  {
+    "revision": "8911aab52d2828ec7e7c",
+    "url": "/react-tetris/static/js/2.b3854255.chunk.js"
+  },
+  {
+    "revision": "45aebee8ce87cf8f24dd",
+    "url": "/react-tetris/static/js/main.a28cafdf.chunk.js"
+  },
+  {
+    "revision": "9f8e8f985e23fe474e69",
+    "url": "/react-tetris/static/js/runtime~main.9a7ed037.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "dcd9ec772f65cc8d4faebfdc5bb1f922",
+    "url": "/react-tetris/static/media/bwlfp7b078w0-background.dcd9ec77.jpg"
+  }
+]);
